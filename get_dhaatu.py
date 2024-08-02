5041c669545eb33a5a046d27a8e0eb9f3eb88f67
Arindam Saha <arindamsaha1507@gmail.com>
2024-08-02 14:05:33 +0100

    Renamed Dhaatu

--- get_dhaatu.py
+++ get_dhaatu.py
@@ -1,32 +1,13 @@
-# import pandas as pd
-from dhaatu_creator import Dhaatu, Prakriya
+from dhaatu import Dhaatu, Prakriya
 
 
-# import yaml
-# from pathlib import Path
-
-# def get_dhaatu(query, find_unique=False):
-
-#     df = pd.read_csv('धातु.csv')
-
-#     for ii in query.keys():
-#         if ii in df.columns:
-#             df = df[df[ii] == query[ii]]
-
-#     idx = df.index.values
-
-#     with open('धातुपाठ_मूल.txt', 'r') as ff:
-#         s = ff.read()
-
-#     s = s.split('\n')
-
-#     return [Dhaatu(s[ii]) for ii in idx]
-
-
-class DhaatuFunctions:
+class LoadDhaatus:
+    """Class to load Dhaatus from a file"""
 
     @staticmethod
     def create_all_dhaatus():
+        """Create all Dhaatus from the file"""
+
         with open("धातुपाठ_मूल.txt", "r", encoding="utf-8") as ff:
             s = ff.read()
 
@@ -36,17 +17,18 @@ class DhaatuFunctions:
 
 
 def main():
+    """Main function"""
 
     pp = Prakriya()
-    # dhaatu = DhaatuFunctions.create_all_dhaatus()[0]
-    # dhaatu = DhaatuFunctions.create_all_dhaatus()[956]
-    # dhaatu = DhaatuFunctions.create_all_dhaatus()[268]
-    # dhaatu = DhaatuFunctions.create_all_dhaatus()[1]
-    # dhaatu = DhaatuFunctions.create_all_dhaatus()[424]
-    # dhaatu = DhaatuFunctions.create_all_dhaatus()[1206]
-    # dhaatu = DhaatuFunctions.create_all_dhaatus()[1647]
-    # dhaatu = DhaatuFunctions.create_all_dhaatus()[2081]
-    dhaatu = DhaatuFunctions.create_all_dhaatus()[1211]
+    dhaatu = LoadDhaatus.create_all_dhaatus()[0]
+    dhaatu = LoadDhaatus.create_all_dhaatus()[956]
+    dhaatu = LoadDhaatus.create_all_dhaatus()[268]
+    dhaatu = LoadDhaatus.create_all_dhaatus()[1]
+    dhaatu = LoadDhaatus.create_all_dhaatus()[424]
+    dhaatu = LoadDhaatus.create_all_dhaatus()[1206]
+    dhaatu = LoadDhaatus.create_all_dhaatus()[1647]
+    dhaatu = LoadDhaatus.create_all_dhaatus()[2081]
+    dhaatu = LoadDhaatus.create_all_dhaatus()[1211]
     dhaatu.add_dhaatu(pp)
     dhaatu.identify_it(pp)
 
